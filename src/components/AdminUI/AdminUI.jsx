3ac0bbe3c89efacd92a960e677bd58e31147a062
Rohithv07 <rohithv63@gmail.com>
2026-07-28 17:53:51 +0530

    Added navbar and added normal components for home, contact and other things

---
+++ src/components/AdminUI/AdminUI.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function AdminUI() {
+    return (
+        <div>
+            <h1>Admin UI</h1>
+            <p>This is the Admin UI page of the application.</p>
+        </div>
+    );
+}
