04a1a3e84bf9234f5bfbbabd4ebe96b8a615345f
Rohithv07 <rohithv63@gmail.com>
2026-08-06 20:34:31 +0530

    Added home page changes

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
-
+import './Home.css';
 export default function Home() {
     return (
-        <div>
-            <h1>Home Page</h1>
-            <p>This is the Home page of the application.</p>
+        <div className="home-container">
+            <div className="home-text-section">
+                <h1 className="home-title">
+                    EMBRACE YOUR<br /> STORY WITH <br /> AADA{' '}
+                    <img
+                        src="/aada-leaf.png"
+                        alt="Aada Leaf Motif"
+                        className="aada-leaf-img"
+                    />
+                </h1>
+
+                <a href="#shop" className="home-shop-btn">
+                    SHOP THE COLLECTION
+                </a>
+            </div>
+
+            <div className="home-image-section">
+                <img src="/women-model.png" alt="Women Model" className="home-hero-image" />
+            </div>
+
         </div>
     );
 }
